4cd34845920bf3c2b53b96d5087d8cdfffe35a29
vit-hug <vitorhugozagli14@gmail.com>
2026-09-14 14:51:04 -0300

    novo codigo

---
+++ dev2.py
@@ -0,0 +1,6 @@
+valor_1 = float(input('digite o primeiro valor: '))
+valor_2 = float(input('digite o segundo valor: '))
+if valor_1 > valor_2:
+    print('o maior valor é o valor 1: ', valor_1)
+else: 
+    print('o maior valor é o valor 2: ', valor_2)
