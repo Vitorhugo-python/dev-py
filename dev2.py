2bfcf8ca4e0acc7dac159d54da1c5843a8c37fd3
vit-hug <vitorhugozagli14@gmail.com>
2026-09-14 16:30:11 -0300

    nova atualização de codigo

--- dev2.py
+++ dev2.py
@@ -1,6 +1,10 @@
-valor_1 = float(input('digite o primeiro valor: '))
-valor_2 = float(input('digite o segundo valor: '))
-if valor_1 > valor_2:
-    print('o maior valor é o valor 1: ', valor_1)
-else: 
-    print('o maior valor é o valor 2: ', valor_2)
+
+while True:
+    valor1 = float(input('digite o primeiro valor: '))
+    valor2 = float(input('digite o segundo valor: '))
+    if valor1 > valor2:
+        print('o maior valor é o valor 1: ', valor1)
+    else: 
+        print('o maior valor é o valor 2: ', valor2)
+    if input('deseja continuar? (s/n): ') == 'n':
+        break  
